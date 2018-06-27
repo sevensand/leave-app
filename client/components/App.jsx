86f8a16730385638ce218674c464b06bda61a569
Bryan Mesias <sevensand29@gmail.com>
2018-06-27 20:59:27 +0300

    displaying react

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 
-const myComponent = (props) => {
-  return(
-    <h1>test</h1>
-  )
+class App extends React.components {
+  render() {
+    return (
+      <h1>1test</h1>
+    );
+  }
 }
-export default myComponent
+export default App;
